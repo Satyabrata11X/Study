be5f6815c40570f6ad4ddb098c75b3b8beeaded3
satyabrata11x <satyabrata11x@gmail.com>
2024-09-05 19:51:27 +0530

    update

--- java/Twod_array.java
+++ java/Twod_array.java
@@ -1,5 +1,27 @@
+
+import java.util.Scanner;
+
 public class Twod_array {
     public static void main(String[] args) {
-        
+       Scanner SC=new Scanner(System.in);
+       int rows=SC.nextInt();
+       int cols=SC.nextInt();
+       int[][] numbers=new int[rows][cols];
+       //input
+       //rows
+       for(int i=0;i<rows;i++){
+       //coulmns
+       for (int j=0; j< cols; j++) {
+           numbers[i][j]=SC.nextInt();
+
+       }
+       }
+       //output
+       for(int i=0;i<rows;i++){
+          for(int j=0;j<cols;j++){
+            System.out.print(numbers[i][j]+" ");
+          }
+          System.out.println();
+       }
     }
 }
