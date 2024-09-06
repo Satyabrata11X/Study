6aff9ca743091c14a5b567c38da0cebb1b6ffc6b
satyabrata11x <satyabrata11x@gmail.com>
2024-09-06 20:11:27 +0530

    update

---
+++ java/SubStrings.java
@@ -0,0 +1,8 @@
+public class SubStrings {
+    public static void main(String[] args) {
+        //substrings
+        String sentence="My name is Satyabrata";
+        String name=sentence.substring(11,sentence.length());
+        System.out.println(name);
+    }
+}
